1e886a4a8fa6a04fe1e88db30999aa9b1fbc382b
shabazrk <shabazrk9@gmail.com>
2023-10-02 18:55:58 +0530

    opening browser

--- src/test/java/gitbash/GitClassTest.java
+++ src/test/java/gitbash/GitClassTest.java
@@ -1,11 +1,24 @@
 package gitbash;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class GitClassTest {
-
-	@Test
-	public void main() {
+WebDriver driver;
+	@Test(alwaysRun = true)
+	@Parameters("BROWSER")
+	public void main(String BROWSER) {
 		System.out.println("1st test case");
+		if(BROWSER.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		else {
+			System.out.println("invalid browser");
+		}
 	}
 }
